13289e99e4a2275d3b598e8ba3ebe375a0e4b8c4
ganghee <idiosauri@naver.com>
2018-07-27 13:53:54 +0900

    설정파일 서버설정, DB스키마 설정, route 설정

--- config/config.js
+++ config/config.js
@@ -20,19 +20,5 @@ module.exports = {
         {file : './coffeeshop', path : '/process/withincoffeeshop2', method : 'findWithin2', type : 'post'},
         {file : './coffeeshop', path : '/process/circlecoffeeshop', method : 'findCircle', type : 'post'}
 	],
-	facebook: {		// passport facebook
-		clientID: '1442860336022433',
-		clientSecret: '13a40d84eb35f9f071b8f09de10ee734',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback'
-	},
-	twitter: {		// passport twitter
-		clientID: 'id',
-		clientSecret: 'secret',
-		callbackURL: '/auth/twitter/callback'
-	},
-	google: {		// passport google
-		clientID: 'id',
-		clientSecret: 'secret',
-		callbackURL: '/auth/google/callback'
-	}
+	
 }
